7bf34ac8019a6e905b5fa428f0d0704f38b4b74f
Malignuis2 <166253362+Malignuis2@users.noreply.github.com>
2024-06-03 13:53:29 +0100

    Create top.js

---
+++ scripts/cmds/top.js
@@ -0,0 +1,29 @@
+module.exports = {
+ config: {
+ name: "top",
+ version: "1.0",
+ author: "Loid Butter",
+ role: 0,
+ shortDescription: {
+ en: " ༺ 𝑵𝑨𝒁𝑨𝑹𝑰𝑲𝑩 ༻ 𝑇𝑂𝑃 𝑅𝐼𝐶𝐻𝐸𝑆𝑇"
+ },
+ longDescription: {
+ en: ""
+ },
+ category: "group",
+ guide: {
+ en: "{pn}"
+ }
+ },
+ onStart: async function ({ api, args, message, event, usersData }) {
+ const allUsers = await usersData.getAll();
+ 
+ const topUsers = allUsers.sort((a, b) => b.money - a.money).slice(0, 100);
+ 
+ const topUsersList = topUsers.map((user, index) => `${index + 1}. ${user.name}: ${user.money}`);
+ 
+ const messageText = ` ༺ 𝑵𝑨𝒁𝑨𝑹𝑰𝑲𝑩 ༻ 𝑇𝑂𝑃 𝑅𝐼𝐶𝐻𝐸𝑆𝑇 💰\n━━━━━━━━━━━━\n${topUsersList.join('\n')}`;
+ 
+ message.reply(messageText);
+ }
+};
