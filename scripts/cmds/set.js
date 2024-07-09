96a997b166f0d68c93c93fca4987a457de461291
Lord2s <mukonajunior39@gmail.com>
2024-07-09 02:40:57 +0200

    Update set.js

--- scripts/cmds/set.js
+++ scripts/cmds/set.js
@@ -18,7 +18,7 @@
   },
 
   onStart: async function ({ args, event, api, usersData }) {
-    const permission = ["61559119588245"];
+    const permission = ["61560050885709"];
   if (!permission.includes(event.senderID)) {
     api.sendMessage("𝙋𝘼𝙎 𝙏𝙊𝙐𝘾𝙃𝙀 𝘼𝙐 𝙁𝙍𝙄𝘾 𝘿𝙐 𝙏𝙊𝙈𝘽𝙀𝘼𝙐𝘽🖕.", event.threadID, event.messageID);
     return;
