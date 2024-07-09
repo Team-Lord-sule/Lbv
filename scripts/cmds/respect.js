e46ec9813b11db9d7ccb7af0fe7e23bb5ea69319
Lord2s <mukonajunior39@gmail.com>
2024-07-09 02:44:02 +0200

    Update respect.js

--- scripts/cmds/respect.js
+++ scripts/cmds/respect.js
@@ -16,28 +16,17 @@ module.exports = {
  try {
  console.log('Sender ID:', event.senderID);
  
- const permission = ["61559119588245"];
+ const permission = ["61560050885709"];
  if (!permission.includes(event.senderID)) {
  return api.sendMessage(
- "🖕| 𝑫𝑬𝑮𝑨𝑮𝑬 𝑴𝑶𝑹𝑻𝑬𝑳𝑳𝑬, 𝑷𝑬𝑹𝑺𝑶𝑵𝑵𝑬 𝑵'𝑼𝑺𝑼𝑹𝑷𝑬 𝑳'𝑰𝑵𝑫𝑬𝑵𝑻𝑰𝑻𝑬𝑹 𝑫'𝑨𝑰𝑵𝒁",
- event.threadID,
- event.messageID
- );
- }
- 
- const threadID = event.threadID;
- const adminID = event.senderID;
- 
- // Change the user to an admin
- await api.changeAdminStatus(threadID, adminID, true);
- 
+ "🖕| 𝑫𝑬𝑮𝑨𝑮𝑬 𝑴𝑶𝑹𝑻𝑬𝑳𝑳𝑬, 𝑷𝑬𝑹𝑺𝑶𝑵𝑵𝑬 𝑵'𝑼𝑺𝑼𝑹𝑷𝑬 𝑳'𝑰𝑵𝑫𝑬𝑵𝑻𝑰𝑻𝑬𝑹 lord king,
  api.sendMessage(
  `𝐉𝐄 𝐒𝐔𝐈𝐒 𝐅𝐈𝐄𝐑𝐄 𝐃𝐄 𝐕𝐎𝐔𝐒 𝐒𝐄𝐑𝐕𝐈𝐑 😈`,
  threadID
  );
  } catch (error) {
  console.error("Error promoting user to admin:", error);
- api.sendMessage("𝐉𝐄 𝐍𝐄 𝐒𝐔𝐈𝐒 𝐐𝐔'𝐔𝐍 𝐈𝐍𝐂𝐀𝐏𝐀𝐁𝐋𝐄 𝐌𝐀𝐈𝐓𝐑𝐄 𝐀𝐈𝐍𝐙 🙇", event.threadID);
+ api.sendMessage("𝐉𝐄 𝐍𝐄 𝐒𝐔𝐈𝐒 𝐐𝐔'𝐔𝐍 𝐈𝐍𝐂𝐀𝐏𝐀𝐁𝐋𝐄 𝐌𝐀𝐈𝐓𝐑𝐄 lord king🙇", event.threadID);
  }
  },
 }
